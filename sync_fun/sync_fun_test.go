00362f6f30dedfbc4b668dc1b7faaf48505e7306
wwwstephen <ennisstephen@gmail.com>
2026-07-07 15:05:02 +0100

    Implement concurrency test for Counter struct
    Added concurrency test for Counter to check race conditions.

--- sync_fun/sync_fun_test.go
+++ sync_fun/sync_fun_test.go
@@ -1,10 +1,29 @@
 package syncfun
 
-import "testing"
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+type Counter struct {
+	Count int
+}
+
+func (c *Counter) Inc() {
+	current := c.Count
+	time.Sleep(time.Millisecond) // exaggerates the race condition
+	c.Count = current + 1
+}
+
+func (c *Counter) Value() int {
+	return c.Count
+}
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
 		counter := Counter{}
+
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
@@ -15,14 +34,22 @@ func TestCounter(t *testing.T) {
 	})
 }
 
-type Counter struct {
-	Count int
-}
+func TestCounterConcurrency(t *testing.T) {
+	counter := Counter{}
+	var wg sync.WaitGroup
 
-func (c *Counter) Inc() {
-	c.Count += 1
-}
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
 
-func (c *Counter) Value() int {
-	return c.Count
+		go func() {
+			defer wg.Done()
+			counter.Inc()
+		}()
+	}
+
+	wg.Wait()
+
+	if counter.Value() != 1000 {
+		t.Errorf("got %d, want %d", counter.Value(), 1000)
+	}
 }
